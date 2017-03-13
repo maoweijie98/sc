79a2b9e74570471feff6d98975b046299fc9d3f1
maoweijie98 <maoweijie98>
2017-03-13 22:31:37 +0800

    modify name

--- sc-service/src/main/java/com/jg/Service/MemberService.java
+++ sc-service/src/main/java/com/jg/Service/MemberService.java
@@ -4,4 +4,5 @@ package com.jg.Service;
  * Created by Administrator on 2017-03-13.
  */
 public interface MemberService {
+    public static  final  String ServerName="com:jg:service:memberService";
 }
